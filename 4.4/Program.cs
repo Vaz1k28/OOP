b426f76cfc7dfc99deb1909e82d97575f4f29fec
Vaz1k28 <159476582+Vaz1k28@users.noreply.github.com>
2024-11-04 20:54:07 +0300

    Add files via upload

---
+++ 4.4/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] a = new int[10] { 1, -5, 3, 4, 5, -6, 7, 8, -9, 10 };
+int sum = 0;
+
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] >= 0)
+    {
+        sum += a[i];
+    }
+}
+
+Console.WriteLine(sum);
