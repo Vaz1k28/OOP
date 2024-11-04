b671f18a9ff91cc0984b69ec9fd2946732ab502f
Vaz1k28 <159476582+Vaz1k28@users.noreply.github.com>
2024-11-04 21:09:40 +0300

    Add files via upload

---
+++ Лаб3/3.6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("--- Введіть число ---");
+int a = int.Parse(Console.ReadLine());
+
+string b = "";
+
+while (a > 0)
+{
+    b = a % 2 + b;
+    a /= 2;
+}
+
+Console.WriteLine("Двійкове представлення: " + b);
