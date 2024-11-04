6bf5107704f760231e46b3492c95223493a88873
Vaz1k28 <159476582+Vaz1k28@users.noreply.github.com>
2024-11-04 21:00:11 +0300

    Add files via upload

---
+++ Лаб4/4.3/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] a = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8 ,9,10 };
+
+
+Console.WriteLine("Парні числа в масиві:");
+for (int i = 0; i < a.Length; i++)
+    if (a [i]%2 ==0)
+        Console.WriteLine(a[i]);
