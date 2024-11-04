5af784c4fbc555ea1b7cf538924b2d9597e52b60
Vaz1k28 <159476582+Vaz1k28@users.noreply.github.com>
2024-11-05 00:11:38 +0300

    Add files via upload

---
+++ Інтерфейси/3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    interface IPrintable
+    {
+        void Print();
+    }
+
+    // Інтерфейс для взаємодії з бібліотекою
+    interface IBorrowable
+    {
+        void BorrowItem();
+        void ReturnItem();
+        bool IsAvailable();
+    }
+
+    // Клас книги
+    class Book : IPrintable, IBorrowable
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        private bool isBorrowed;
+
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            isBorrowed = false;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, Year: {Year}, Available: {(!isBorrowed ? "Yes" : "No")}");
+        }
+
+        public void BorrowItem()
+        {
+            if (!isBorrowed)
+            {
+                isBorrowed = true;
+                Console.WriteLine($"Book '{Title}' has been borrowed.");
+            }
+            else
+            {
+                Console.WriteLine($"Book '{Title}' is already borrowed.");
+            }
+        }
+
+        public void ReturnItem()
+        {
+            if (isBorrowed)
+            {
+                isBorrowed = false;
+                Console.WriteLine($"Book '{Title}' has been returned.");
+            }
+            else
+            {
+                Console.WriteLine($"Book '{Title}' is not currently borrowed.");
+            }
+        }
+
+        public bool IsAvailable()
+        {
+            return !isBorrowed;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Book book1 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951);
+            Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+
+            // Тестування інтерфейсу IPrintable
+            Console.WriteLine("Book 1 Information:");
+            book1.Print();
+            Console.WriteLine("\nBook 2 Information:");
+            book2.Print();
+
+            // Тестування інтерфейсу IBorrowable
+            Console.WriteLine("\nBorrowing Book 1:");
+            book1.BorrowItem();
+
+            Console.WriteLine("\nAttempting to Borrow Book 1 Again:");
+            book1.BorrowItem();
+
+            Console.WriteLine("\nReturning Book 1:");
+            book1.ReturnItem();
+
+            Console.WriteLine("\nAvailability of Book 1: " + (book1.IsAvailable() ? "Yes" : "No"));
+        }
+    }
+}
---
+++ Інтерфейси/Lab8/Program.cs
@@ -0,0 +1,87 @@
+﻿using Lab8;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Тестування
+        Car car = new Car { Model = "Toyota Camry", Year = 2023 };
+        Motorcycle motorcycle = new Motorcycle();
+        Truck truck = new Truck { CarryingCapacity = 10000 };
+
+        car.Start();
+        motorcycle.Drive();
+        truck.Stop();
+
+     
+
+
+    }
+}
+
+public interface IVehicle
+{
+    void Start();
+    void Stop();
+    void Drive();
+}
+
+public class Car : IVehicle
+{
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    public void Start()
+    {
+        Console.WriteLine($"Автомобіль {Model} ({Year}) запущено.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Автомобіль зупинено.");
+    }
+
+    public void Drive()
+    {
+        Console.WriteLine("Автомобіль їде.");
+    }
+}
+
+public class Motorcycle : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Мотоцикл запущено.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Мотоцикл зупинено.");
+    }
+
+    public void Drive()
+    {
+        Console.WriteLine("Мотоцикл їде.");
+    }
+}
+
+public class Truck : IVehicle
+{
+    public int CarryingCapacity { get; set; }
+
+    public void Start()
+    {
+        Console.WriteLine("Вантажівка запущено.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Вантажівка зупинено.");
+    }
+
+    public void Drive()
+    {
+        Console.WriteLine("Вантажівка їде.");
+    }
+}
