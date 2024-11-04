f8777f4d04afb49899498356854bcd1d0f4ae16c
Vaz1k28 <159476582+Vaz1k28@users.noreply.github.com>
2024-11-04 21:01:21 +0300

    Add files via upload

---
+++ Лаб4/4.5/Program.cs
@@ -0,0 +1,3 @@
+﻿int[] a = new int[10];
+for (int i = a.Length; i < a.Length; i--)
+    Console.WriteLine(i);
---
+++ Лаб4/4.7/Program.cs
@@ -0,0 +1,9 @@
+﻿int [,] a = new int[2, 2];
+
+a[0, 0] = 6;
+a[1, 1] = 4;
+
+int sum = a[0, 0] + a[1, 1];
+Console.WriteLine(sum);
+
+
