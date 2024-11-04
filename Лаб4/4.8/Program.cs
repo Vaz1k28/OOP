0e84a2144a821010d4e3b7b219d2b06f776f44b7
Vaz1k28 <159476582+Vaz1k28@users.noreply.github.com>
2024-11-04 21:03:43 +0300

    Add files via upload

---
+++ Лаб4/4.8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("--- Введіть який стовбець матриці порахувати ---");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] a = {
+    {4 ,5, 6,},
+    {5 ,3, -2,},
+    {7, 1, 0,},
+};
+int sum = 1;
+n--;
+for (int i = 1; i < 3; i++)
+{
+    sum *= a[i, n];
+    n++;
+}
+Console.WriteLine($"Добуток елементів стовпця {n + 1} = {sum}");
+
+
+
---
+++ Лаб4/Лаб4/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] a = new int[5] { 1, 6, 5, 7, 2 };
+int sum = 0;
+
+for (int i = 0; i < a.Length; i++)
+    sum += a[i];
+
+double average = (double)sum / a.Length;
+Console.WriteLine("Cереднє арифметичне: " + average);
