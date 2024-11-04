b0721bcbb2b42f93939eea6a9575859df1636fb4
Vaz1k28 <159476582+Vaz1k28@users.noreply.github.com>
2024-11-04 22:12:40 +0300

    Add files via upload

---
+++ Робота з колекціями/Lab9/2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UserManager
+    {
+        private List<User> users = new List<User>();
+
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        public void RemoveUser(int userId)
+        {
+            users.RemoveAll(user => user.id == userId);
+        }
+
+        public User GetUserById(int userId)
+        {
+            return users.FirstOrDefault(user => user.id == userId);
+        }
+
+        public void PrintAllUsers()
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Id: {user.id}, Name: {user.Name}");
+            }
+        }
+    }
+}
